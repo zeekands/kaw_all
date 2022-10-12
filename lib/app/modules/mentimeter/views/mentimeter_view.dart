9e61cfc43f6412defc561dfcbec432b6f696488e
akhes24 <akhes19ti@mahasiswa.pcr.ac.id>
2022-10-12 17:17:33 +0700

    update mentimeter page

--- lib/app/modules/mentimeter/views/mentimeter_view.dart
+++ lib/app/modules/mentimeter/views/mentimeter_view.dart
@@ -11,56 +11,320 @@ class MentimeterView extends GetView<MentimeterController> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Stack(
-          children: [
-            Image.asset(
-              'assets/images/fixbg.png',
+        child: Container(
+          width: 1.sw,
+          height: 1.sh,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/background_mentimeter.png'),
               fit: BoxFit.cover,
-              height: 1.sh,
-              width: 1.sw,
             ),
-            Column(
-              children: [
-                Image.asset(
-                  'assets/images/top_bar_mentimeter.png',
-                  width: 1.sw,
-                  fit: BoxFit.cover,
-                ),
-                Image.asset(
-                  'assets/images/danger_zone_card.png',
-                  width: 355.w,
-                  height: 255.h,
-                ),
-                const Spacer(),
-                Container(
-                  height: .3.sh,
-                  width: 1.sw,
-                  decoration: const BoxDecoration(
-                    color: Color(0xffD8E6D2),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30),
+          ),
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    width: 1.sw,
+                    height: 247.h,
+                    decoration: BoxDecoration(
+                      color: const Color(0xffE7D69C),
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(40),
+                        bottomRight: Radius.circular(40),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: const Color(0xff000000).withOpacity(0.25),
+                          spreadRadius: 0,
+                          blurRadius: 4,
+                          offset:
+                              const Offset(0, 2), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Image.asset(
+                              'assets/images/logo.png',
+                              width: 42.w,
+                            ),
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/images/icon_more_appbar.png',
+                                  width: 24.w,
+                                ),
+                                10.horizontalSpace,
+                                Image.asset(
+                                  'assets/images/profile_picture.png',
+                                  width: 42.w,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ).paddingSymmetric(horizontal: 12.w, vertical: 14.h),
+                        13.verticalSpace,
+                        Container(
+                          width: 328.w,
+                          height: 120.h,
+                          decoration: BoxDecoration(
+                            color: const Color(0xffF7F7F7),
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                flex: 1,
+                                child: Container(
+                                  height: 120.h,
+                                  decoration: const BoxDecoration(
+                                    color: Color(0xffACBCA5),
+                                    borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(25),
+                                      bottomLeft: Radius.circular(25),
+                                    ),
+                                  ),
+                                  child: Image.asset(
+                                    'assets/images/img_card_daerah_rawan.png',
+                                    height: 30.h,
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 2,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Rawan, Kawan!",
+                                      style: TextStyle(
+                                        fontFamily: 'JosefinSans',
+                                        fontSize: 20.sp,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    5.verticalSpace,
+                                    Text(
+                                      "Tau daerah yang rawan? Share it here!",
+                                      style: TextStyle(
+                                        fontFamily: 'JosefinSans',
+                                        fontSize: 14.sp,
+                                      ),
+                                    ),
+                                    10.verticalSpace,
+                                    Image.asset(
+                                      'assets/images/arrow_2.png',
+                                      width: 24.w,
+                                    ),
+                                  ],
+                                ).paddingAll(10),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Flexible(
-                        child: Image.asset(
-                          "assets/images/fake_call.png",
-                        ),
-                      ),
-                      Flexible(
-                        child: Image.asset(
-                          "assets/images/berani_melapor.png",
-                        ),
-                      ),
-                    ],
-                  ).paddingOnly(left: 10.w),
+                ],
+              ),
+              const Spacer(),
+              Container(
+                width: 355.w,
+                height: 235.h,
+                decoration: BoxDecoration(
+                  color: const Color(0xffF7F7F7),
+                  borderRadius: BorderRadius.circular(25),
                 ),
-              ],
-            ),
-          ],
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: ClipRRect(
+                        borderRadius: const BorderRadius.only(
+                          bottomLeft: Radius.circular(25),
+                          bottomRight: Radius.circular(25),
+                        ),
+                        child: Image.asset(
+                          'assets/images/img_card_danger_zone.png',
+                          width: 370.w,
+                          height: 180.h,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "Danger Zones",
+                      style: TextStyle(
+                        color: const Color(0xffA14D4D),
+                        fontFamily: 'JosefinSans',
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ).paddingOnly(top: 20.h, left: 30.w),
+                  ],
+                ),
+              ),
+              const Spacer(),
+              Container(
+                width: 1.sw,
+                height: 250.h,
+                decoration: BoxDecoration(
+                  color: const Color(0xffD8E6D2),
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color(0xff000000).withOpacity(0.25),
+                      spreadRadius: 0,
+                      blurRadius: 4,
+                      offset: const Offset(0, 2), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          "Need a friend?",
+                          style: TextStyle(
+                            color: const Color(0xff585656),
+                            fontFamily: 'JosefinSans',
+                            fontSize: 20.sp,
+                            fontWeight: FontWeight.bold,
+                            shadows: [
+                              Shadow(
+                                color:
+                                    const Color(0xff000000).withOpacity(0.25),
+                                offset: const Offset(0, 2),
+                                blurRadius: 4,
+                              ),
+                            ],
+                          ),
+                        ).paddingOnly(left: 29.w, top: 15.h),
+                        10.verticalSpace,
+                        Container(
+                          width: 142.w,
+                          height: 185.h,
+                          decoration: BoxDecoration(
+                            color: const Color(0xffF4DFD9),
+                            borderRadius: BorderRadius.circular(25),
+                            boxShadow: [
+                              BoxShadow(
+                                color:
+                                    const Color(0xff000000).withOpacity(0.25),
+                                spreadRadius: 0,
+                                blurRadius: 2,
+                                offset: const Offset(
+                                    0, 2), // changes position of shadow
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Just call us with Fake Call",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: const Color(0xffFFFEFE),
+                                  fontFamily: 'JosefinSans',
+                                  fontSize: 14.sp,
+                                  fontWeight: FontWeight.bold,
+                                  shadows: [
+                                    Shadow(
+                                      color: const Color(0xff000000)
+                                          .withOpacity(0.25),
+                                      offset: const Offset(0, 2),
+                                      blurRadius: 2,
+                                    ),
+                                  ],
+                                ),
+                              ).paddingAll(10),
+                              Image.asset(
+                                'assets/images/call_dashboard.png',
+                                width: 65.w,
+                              ),
+                            ],
+                          ),
+                        ).paddingOnly(left: 29.w),
+                      ],
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            "Berani melapor",
+                            style: TextStyle(
+                              color: const Color(0xff585656),
+                              fontFamily: 'JosefinSans',
+                              fontSize: 20.sp,
+                              fontWeight: FontWeight.bold,
+                              shadows: [
+                                Shadow(
+                                  color:
+                                      const Color(0xff000000).withOpacity(0.25),
+                                  offset: const Offset(0, 2),
+                                  blurRadius: 4,
+                                ),
+                              ],
+                            ),
+                          ).paddingOnly(right: 19.w, top: 35.h),
+                          Text(
+                            "bersama UPT PPA",
+                            style: TextStyle(
+                              color: const Color(0xff585656),
+                              fontFamily: 'JosefinSans',
+                              fontSize: 8.sp,
+                              shadows: [
+                                Shadow(
+                                  color:
+                                      const Color(0xff000000).withOpacity(0.25),
+                                  offset: const Offset(0, 2),
+                                  blurRadius: 4,
+                                ),
+                              ],
+                            ),
+                          ).paddingOnly(right: 19.w, top: 5.h, bottom: 10.h),
+                          Container(
+                            width: 200.w,
+                            height: 145.h,
+                            decoration: BoxDecoration(
+                              color: const Color(0xffF2E9C8),
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(25),
+                                bottomLeft: Radius.circular(25),
+                              ),
+                              boxShadow: [
+                                BoxShadow(
+                                  color:
+                                      const Color(0xff000000).withOpacity(0.25),
+                                  spreadRadius: 0,
+                                  blurRadius: 2,
+                                  offset: const Offset(
+                                      0, 2), // changes position of shadow
+                                ),
+                              ],
+                            ),
+                            child: Image.asset(
+                                    "assets/images/img_card_berani_melapor.png")
+                                .paddingOnly(top: 25.h),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
